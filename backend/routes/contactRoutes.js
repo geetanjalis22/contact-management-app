e3df8c13de056b16a6a883c8f7ccf3708a19ac04
Geetanjali <72863114+geetanjalis22@users.noreply.github.com>
2026-01-03 12:38:37 +0530

    Backend Files Added

---
+++ backend/routes/contactRoutes.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const Contact = require("../models/Contact");
+const router = express.Router();
+
+// POST contact
+router.post("/", async (req, res) => {
+  try {
+    const contact = new Contact(req.body);
+    await contact.save();
+    res.status(201).json(contact);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+// GET contacts
+router.get("/", async (req, res) => {
+  const contacts = await Contact.find().sort({ createdAt: -1 });
+  res.json(contacts);
+});
+
+// DELETE contact (Bonus)
+router.delete("/:id", async (req, res) => {
+  await Contact.findByIdAndDelete(req.params.id);
+  res.json({ message: "Contact deleted" });
+});
+
+module.exports = router;
